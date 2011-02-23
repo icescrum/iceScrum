8e6adc97b03c039f019f9f1814adc9f21d54e996
nico <Nico@.(none)>
2011-02-23 16:10:08 +0100

    update version icescrum-core fuck !

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -75,7 +75,7 @@ grails.project.dependency.resolution = {
   }
 
   plugins {
-      compile "org.icescrum:icescrum-core:1.2"
+      compile "org.icescrum:icescrum-core:1.2.1"
       compile "org.icescrum:entry-points:0.2"
       compile "org.icescrum:icepush-jquery:0.2"
   }
