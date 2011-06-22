e01bd93f264926b8ce9a929ca61dfcd19eb26815
Vincent Barrier <vincent.barrier@icescrum.com>
2011-06-22 18:35:50 +0200

    events

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,3 +1,10 @@
+import grails.util.PluginBuildSettings
+import java.util.regex.Matcher
+import grails.util.GrailsNameUtils
+import org.codehaus.groovy.grails.cli.GrailsScriptRunner
+import org.apache.catalina.connector.Connector
+import org.apache.tools.ant.taskdefs.Ant
+
 /*
 * Copyright (c) 2010 iceScrum Technologies.
 *
@@ -15,9 +22,6 @@
 * You should have received a copy of the GNU Affero General Public License
 * along with iceScrum.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-import org.apache.tools.ant.taskdefs.Ant;
-
 
 eventCreateWarStart = {warname, stagingDir ->
     Ant.propertyfile(file: "${stagingDir}/WEB-INF/classes/application.properties") {
@@ -47,4 +51,13 @@ def getRevision() {
     }
 
     return scmVersion ?: 'UNKNOWN'
+}
+
+eventConfigureTomcat = {tomcat ->
+    def ajpConnector = new Connector("org.apache.coyote.http11.Http11NioProtocol")
+    ajpConnector.port = 8009
+    ajpConnector.setProperty("redirectPort", "8443")
+    ajpConnector.setProperty("protocol", "AJP/1.3")
+    ajpConnector.setProperty("enableLookups", "false")
+    tomcat.service.addConnector ajpConnector
 }
