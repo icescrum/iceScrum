642e995855273842723626b33a74895c24288a38
Vincent Barrier <vincent.barrier@icescrum.com>
2011-08-08 21:11:59 +0200

    update version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -78,7 +78,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        compile "org.icescrum:icescrum-core:1.4.2.11"
+        compile "org.icescrum:icescrum-core:1.4.2.12"
         compile "org.icescrum:entry-points:0.3-BETA"
         compile ":cache-headers:1.1.5"
         compile ":cached-resources:1.0"
