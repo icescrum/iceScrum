d86b81d9e8f51bf7eadd0e0c462bc28d6a34378e
Vincent Barrier <vincent.barrier@icescrum.com>
2011-08-23 12:41:51 +0200

    testing for jenkins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -65,7 +65,7 @@ grails.project.dependency.resolution = {
         mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         mavenRepo "http://repository.codehaus.org"
-        mavenRepo "http://repo.icescrum.org/artifactory/plugins-release/"
+        //mavenRepo "http://repo.icescrum.org/artifactory/plugins-release/"
         mavenRepo "http://repo.icescrum.org/artifactory/plugins-snapshot/"
     }
 
