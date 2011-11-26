d68b3bc330f8fd5236788b1b145dfaf2576ccb03
Vincent Barrier <vincent.barrier@icescrum.com>
2011-11-26 01:08:19 +0100

    surf between grails resources / spring security bugs

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -102,7 +102,7 @@ grails.project.dependency.resolution = {
         compile ":hibernate:1.3.7"
         compile ":jquery:1.6.1.1"
         compile ":jquery-ui:1.8.11"
-        compile ":resources:1.1.1"
+        compile ":resources:1.1"
         compile ":session-temp-files:1.0"
         compile ":wikitext:0.1.2"
         compile ":zipped-resources:1.0"
