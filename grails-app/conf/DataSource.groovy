e06ca3ece2697307c97a24103b1a7107603c61f8
Vincent Barrier <vincent.barrier@icescrum.com>
2012-05-09 14:23:04 +0200

    revert config

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -43,8 +43,8 @@ environments {
             username="root"
             password="root"
             dbCreate = "update"*/
-            dbCreate = "update" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:file:prodDba"
+            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+            url = "jdbc:hsqldb:file:devDba"
             loggingSql = false
         }
     }
