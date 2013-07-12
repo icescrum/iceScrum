19c0a3c1309e1315df35fbb3b3e7727156f99177
Vincent Barrier <vbarrier@kagilum.com>
2013-07-12 15:25:33 +0200

    remove IE7-IE8 compatibility

--- web-app/js/jquery/jquery.history.js
+++ web-app/js/jquery/jquery.history.js
@@ -90,58 +90,6 @@
       timeout_id = setTimeout( poll, $.fn[ str_hashchange ].delay );
     };
 
-    // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    // vvvvvvvvvvvvvvvvvvv REMOVE IF NOT SUPPORTING IE6/7/8 vvvvvvvvvvvvvvvvvvv
-    // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    $.browser.msie && !supports_onhashchange && (function(){
-      var iframe,
-        iframe_src;
-      self.start = function(){
-        if ( !iframe ) {
-          iframe_src = $.fn[ str_hashchange ].src;
-          iframe_src = iframe_src && iframe_src + get_fragment();
-          iframe = $('<iframe tabindex="-1" title="empty"/>').hide()
-            .one( 'load', function(){
-              iframe_src || history_set( get_fragment() );
-              poll();
-            })
-            .attr( 'src', iframe_src || 'javascript:0' )
-
-            .insertAfter( 'body' )[0].contentWindow;
-
-          doc.onpropertychange = function(){
-            try {
-              if ( event.propertyName === 'title' ) {
-                iframe.document.title = doc.title;
-              }
-            } catch(e) {}
-          };
-
-        }
-      };
-      self.stop = fn_retval;
-
-      history_get = function() {
-        return get_fragment( iframe.location.href );
-      };
-
-      history_set = function( hash, history_hash ) {
-        var iframe_doc = iframe.document,
-          domain = $.fn[ str_hashchange ].domain;
-        if ( hash !== history_hash ) {
-          iframe_doc.title = doc.title;
-          iframe_doc.open();
-          domain && iframe_doc.write( '<script>document.domain="' + domain + '"</script>' );
-          iframe_doc.close();
-          iframe.location.hash = hash;
-        }
-      };
-
-    })();
-    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    // ^^^^^^^^^^^^^^^^^^^ REMOVE IF NOT SUPPORTING IE6/7/8 ^^^^^^^^^^^^^^^^^^^
-    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
     return self;
   })();
 
