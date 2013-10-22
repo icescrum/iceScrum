6750b6374f9e5f3db7c32ae309d9cb95a6d01437
noullet <nnoullet@kagilum.com>
2013-10-22 20:14:03 +0200

    T771-0 T772-0 Change current user and update tasks, finally :) Task display includes ID, creator, responsible, descriptio ad notes Update includes (block / unblock, take / release, copy, delete)

--- web-app/js/icescrum/jquery.icescrum.functions.js
+++ web-app/js/icescrum/jquery.icescrum.functions.js
@@ -810,6 +810,7 @@
                 },
 
                 task: {
+                    i18n:{},
                     STATE_WAIT:0,
                     STATE_BUSY:1,
                     STATE_DONE:2,
@@ -831,6 +832,83 @@
                             constraintTmpl:function() {
                                 var filter = $.icescrum.sprint.taskFilters[$.icescrum.sprint.currentTaskFilter];
                                 return (filter == undefined) || filter(this);
+                            },
+                            update:function(template) {
+                                var tmpl = $.icescrum.task.templates[template];
+                                // Save selection
+                                var selectedTasks = $(tmpl.selector + '.ui-selected').map(function() {
+                                    return $(this).data('elemid');
+                                });
+                                $(this).each(function() {
+                                    $.icescrum.task.remove.apply(this, [template]);
+                                    $.icescrum.task.add.apply(this, [template]);
+                                });
+                                // Restore selection
+                                selectedTasks.each(function() {
+                                    var elem = $(tmpl.selector + '[data-elemid=' + this + ']');
+                                    if (elem.length > 0 && !elem.hasClass('ui-selected')) {
+                                        elem.addClass('ui-selected');
+                                    }
+                                });
+                            },
+                            _postRendering:function(tmpl, postit) {
+
+                                if ($(tmpl.view + '-' + this.sprint.id).hasClass('ui-selectable') && this.sprint.state != $.icescrum.sprint.STATE_DONE) {
+                                    postit.addClass('ui-selectee');
+                                }
+                                if (this.totalAttachments == undefined || !this.totalAttachments) {
+                                    postit.find('.postit-attachment').hide();
+                                }
+                                if (this.totalComments <= 0 ) {
+                                    postit.find('.postit-comment').hide();
+                                }
+
+                                var responsible = (this.responsible && this.responsible.id == $.icescrum.user.id) ? true : false;
+                                var creator = this.creator.id == $.icescrum.user.id;
+                                var taskDone = this.state == $.icescrum.task.STATE_DONE;
+                                var notStoryDone = !this.parentStory || this.parentStory.state != $.icescrum.story.STATE_DONE;
+
+                                var taskEditable = ($.icescrum.user.scrumMaster || responsible || creator) && !taskDone;
+                                var taskDeletable = $.icescrum.user.scrumMaster || responsible || creator;
+                                var taskBlockable = ($.icescrum.user.scrumMaster || responsible) && !taskDone && this.sprint.state == $.icescrum.sprint.STATE_INPROGRESS;
+                                var taskSortable = ($.icescrum.user.scrumMaster || responsible || (!this.responsible && $.icescrum.user.inProduct() && $.icescrum.product.assignOnBeginTask && this.state == $.icescrum.task.STATE_WAIT)) && notStoryDone;
+                                var taskTakable = !responsible && !taskDone;
+                                var taskReleasable = responsible && !taskDone;
+                                var taskCopyable = notStoryDone;
+
+                                if(this.sprint.state == $.icescrum.sprint.STATE_DONE) {
+                                    $('.dropmenu-action', postit).remove();
+                                } else {
+                                    // Menu
+                                    if (!taskEditable) {
+                                        $('#menu-edit-' + this.id, postit).remove();
+                                    } else {
+                                        $('.mini-value', postit).addClass('editable editable-hover');
+                                    }
+                                    if (!taskDeletable) {
+                                        $('#menu-delete-' + this.id, postit).remove();
+                                    }
+                                    if (!taskBlockable) {
+                                        $('#menu-blocked-' + this.id, postit).remove();
+                                    }
+                                    if (!taskTakable) {
+                                        $('#menu-take-' + this.id, postit).remove();
+                                    }
+                                    if (!taskReleasable) {
+                                        $('#menu-unassign-' + this.id, postit).remove();
+                                    }
+                                    if (!taskCopyable) {
+                                        $('#menu-copy-' + this.id, postit).remove();
+                                    }
+                                    // Draggable
+                                    if (taskSortable) {
+                                        $('.postit-label', postit).addClass('postit-sortable');
+                                    }
+                                }
+
+                                this.blocked ? $('#menu-blocked-' + this.id + ' a', postit).text($.icescrum.task.UNBLOCK) : $('#menu-blocked-' + this.id + ' a', postit).text($.icescrum.task.BLOCK);
+
+                                $.icescrum.sprint.updateRemaining();
                             }
                         }
                     },
@@ -841,86 +919,31 @@
                     },
 
                     update:function(template) {
-                        $(this).each(function() {
-                            $.icescrum.task.remove.apply(this, [template]);
-                            $.icescrum.task.add.apply(this, [template]);
-                        });
+                        var tmpl = $.icescrum.task.templates[template];
+                        tmpl.update.apply(this, [template]);
                     },
 
                     remove:function(template) {
+                        var tmpl = $.icescrum.task.templates[template];
                         $(this).each(function() {
-                            $.icescrum.task.templates[template].remove.apply(this);
+                            tmpl.remove.apply(this, [tmpl]);
                         });
                         $.icescrum.sprint.updateRemaining();
                     },
 
-                    _postRendering:function(tmpl, postit) {
-
-                        if ($(tmpl.view + '-' + this.sprint.id).hasClass('ui-selectable') && this.sprint.state != $.icescrum.sprint.STATE_DONE) {
-                            postit.addClass('ui-selectee');
-                        }
-                        if (this.totalAttachments == undefined || !this.totalAttachments) {
-                            postit.find('.postit-attachment').hide();
-                        }
-                        if (this.totalComments <= 0 ) {
-                            postit.find('.postit-comment').hide();
-                        }
-
-                        var responsible = (this.responsible && this.responsible.id == $.icescrum.user.id) ? true : false;
-                        var creator = this.creator.id == $.icescrum.user.id;
-                        var taskDone = this.state == $.icescrum.task.STATE_DONE;
-                        var notStoryDone = !this.parentStory || this.parentStory.state != $.icescrum.story.STATE_DONE;
-
-                        var taskEditable = ($.icescrum.user.scrumMaster || responsible || creator) && !taskDone;
-                        var taskDeletable = $.icescrum.user.scrumMaster || responsible || creator;
-                        var taskBlockable = ($.icescrum.user.scrumMaster || responsible) && !taskDone && this.sprint.state == $.icescrum.sprint.STATE_INPROGRESS;
-                        var taskSortable = ($.icescrum.user.scrumMaster || responsible || (!this.responsible && $.icescrum.user.inProduct() && $.icescrum.product.assignOnBeginTask && this.state == $.icescrum.task.STATE_WAIT)) && notStoryDone;
-                        var taskTakable = !responsible && !taskDone;
-                        var taskReleasable = responsible && !taskDone;
-                        var taskCopyable = notStoryDone
-
-                        if(this.sprint.state == $.icescrum.sprint.STATE_DONE) {
-                            $('.dropmenu-action', postit).remove();
-                        } else {
-                            // Menu
-                            if (!taskEditable) {
-                                $('#menu-edit-' + this.id, postit).remove();
-                            } else {
-                                $('.mini-value', postit).addClass('editable editable-hover');
-                            }
-                            if (!taskDeletable) {
-                                $('#menu-delete-' + this.id, postit).remove();
-                            }
-                            if (!taskBlockable) {
-                                $('#menu-blocked-' + this.id, postit).remove();
-                            }
-                            if (!taskTakable) {
-                                $('#menu-take-' + this.id, postit).remove();
-                            }
-                            if (!taskReleasable) {
-                                $('#menu-unassign-' + this.id, postit).remove();
-                            }
-                            if (!taskCopyable) {
-                                $('#menu-copy-' + this.id, postit).remove();
-                            }
-                            // Draggable
-                            if (taskSortable) {
-                                $('.postit-label', postit).addClass('postit-sortable');
-                            }
-                        }
-
-                        this.blocked ? $('#menu-blocked-' + this.id + ' a', postit).text($.icescrum.task.UNBLOCK) : $('#menu-blocked-' + this.id + ' a', postit).text($.icescrum.task.BLOCK);
-
-                        $.icescrum.sprint.updateRemaining();
+                    _postRendering:function(tmpl, domObject) {
+                        tmpl._postRendering.apply(this, [tmpl, domObject]);
                     },
 
                     toggleBlocked:function(data) {
                         var $elem = $('#postit-task-' + data.id + ' .postit-ico,.table-line[data-elemid=' + data.id + ']');
                         if ($elem.toggleClass('ico-task-1').hasClass('ico-task-1')) {
                             $('#menu-blocked-' + data.id + ' a').text($.icescrum.task.UNBLOCK);
+                            $('#task-action-blocked-' + data.id + ' a .content').text($.icescrum.task.UNBLOCK);
                             $elem.attr('title', $.icescrum.task.BLOCKED);
                         } else {
                             $('#menu-blocked-' + data.id + ' a').text($.icescrum.task.BLOCK);
+                            $('#task-action-blocked-' + data.id + ' a .content').text($.icescrum.task.BLOCK);
                             $elem.attr('title', '');
                         }
                     }
@@ -1536,7 +1559,10 @@
 
                 addOrUpdate:function(object, _tmpl, after, append) {
 
-                    object = $.icescrum.htmlEncodeJSON(object);
+                    // Encoding of pushed objects is disabled temporarily
+                    // because it causes issues with HTML content (notes etc.)
+                    // A manual decoding will be required for those elements
+                    // object = $.icescrum.htmlEncodeJSON(object);
 
                     if ($.isFunction(_tmpl.constraintTmpl)) {
                         if (_tmpl.constraintTmpl.apply(object) == false) {
