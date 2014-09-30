4f2c2f8460b68baac573b4ba284e4ba5c4501b2f
Nicolas Noullet <nnoullet@kagilum.com>
2014-09-30 15:14:34 +0200

    Grails 2 more config and plugin update

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -72,7 +72,7 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        runtime 'mysql:mysql-connector-java:5.1.18'
+        runtime 'mysql:mysql-connector-java:5.1.32'
     }
 
     plugins {
