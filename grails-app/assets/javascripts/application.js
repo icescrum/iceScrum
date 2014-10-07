09b752f8684af24262f9bc4a2847f3bfaeadb5fc
Nicolas Noullet <nnoullet@kagilum.com>
2014-10-07 11:41:24 +0200

    Resources

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -22,32 +22,32 @@
  *
  */
 
-//= encoding UTF-8
+//= encoding          UTF-8
 
-//= require vendors/jquery/jquery-1.11.0.min
-//= require vendors/jquery/jquery-migrate-1.2.1.min
-//= require vendors/jquery-jqplot/jquery.jqplot.min
+//= require           vendors/jquery/jquery-1.11.0.min
+//= require           vendors/jquery/jquery-migrate-1.2.1.min
+//= require           vendors/jquery-jqplot/jquery.jqplot.min
 //= require_full_tree vendors/jquery
 
-//= require vendors/bootstrap/bootstrap.min
+//= require           vendors/bootstrap/bootstrap.min
 
-//= require vendors/jquery-ui/jquery-ui-1.10.4.custom.min
+//= require           vendors/jquery-ui/jquery-ui-1.10.4.custom.min
 
-//= require vendors/angular/angular.min
-//= require vendors/angular/angular-route.min
-//= require vendors/angular/angular-resource.min
+//= require           vendors/angular/angular.min
+//= require           vendors/angular/angular-route.min
+//= require           vendors/angular/angular-resource.min
 
-//= require vendors/angular/plugins/ng-pdfviewer.min
+//= require           vendors/angular/plugins/ng-pdfviewer.min
 //= require_full_tree vendors/i18n
 
 //= require_full_tree vendors
 
-//= require icescrum/jquery.icescrum.js
+//= require           icescrum/jquery.icescrum.js
 //= require_full_tree icescrum
 
-//= require app/app
-//= require app/controllers
-//= require app/directives
-//= require app/filters
-//= require app/services
+//= require           app/app
+//= require           app/controllers
+//= require           app/directives
+//= require           app/filters
+//= require           app/services
 //= require_full_tree app
