2cdfc281e25e3fed9951e186bc818a07fabde419
Nicolas Noullet <nnoullet@kagilum.com>
2014-10-07 10:04:03 +0200

    Grails 2 - Update iceScrum resources

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -1,7 +1,33 @@
+/*
+ * Copyright (c) 2014 Kagilum SAS.
+ *
+ * This file is part of iceScrum.
+ *
+ * iceScrum is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * iceScrum is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with iceScrum.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *
+ * Vincent Barrier (vbarrier@kagilum.com)
+ * Nicolas Noullet (nnoullet@kagilum.com)
+ *
+ */
+
+//= encoding UTF-8
+
 //= require vendors/jquery/jquery-1.11.0.min
 //= require vendors/jquery/jquery-migrate-1.2.1.min
 //= require vendors/jquery-jqplot/jquery.jqplot.min
-//= require_tree vendors/jquery
+//= require_full_tree vendors/jquery
 
 //= require vendors/bootstrap/bootstrap.min
 
@@ -12,18 +38,16 @@
 //= require vendors/angular/angular-resource.min
 
 //= require vendors/angular/plugins/ng-pdfviewer.min
-//= require_tree vendors/i18n
+//= require_full_tree vendors/i18n
 
-//= require_tree vendors
+//= require_full_tree vendors
 
 //= require icescrum/jquery.icescrum.js
-//= require_tree icescrum
+//= require_full_tree icescrum
 
 //= require app/app
 //= require app/controllers
 //= require app/directives
 //= require app/filters
 //= require app/services
-//= require_tree app
-
-//= encoding UTF-8
+//= require_full_tree app
