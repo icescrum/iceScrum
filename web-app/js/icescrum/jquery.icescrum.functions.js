78cd65c1c1a0d392558f1256308492a1d7de1a1a
vbarrier <vbarrier@kagilum.com>
2015-07-22 08:51:24 +0200

    fix url and blocked "next" button

--- web-app/js/icescrum/jquery.icescrum.functions.js
+++ web-app/js/icescrum/jquery.icescrum.functions.js
@@ -271,7 +271,7 @@
                             $teamName.val('');
                             $findMembers.hide();
                         }
-                        $.getJSON($.icescrum.o.grailsServer + '/members/getTeamMembers/' + teamId, function(members) {
+                        $.getJSON($.icescrum.o.baseUrl + 'members/getTeamMembers/' + teamId, function(members) {
                             $.each(members, function(index, member) {
                                 member.editable = newTeam;
                                 member.view = 'members';
