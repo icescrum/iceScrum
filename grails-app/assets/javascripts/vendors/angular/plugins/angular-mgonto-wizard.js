12cbf4ff97bf2d64dafa41555bd2a2ef7f835e80
vbarrier <vbarrier@kagilum.com>
2016-05-12 09:51:00 +0100

    fix style & update plugin

--- grails-app/assets/javascripts/vendors/angular/plugins/angular-mgonto-wizard.js
+++ grails-app/assets/javascripts/vendors/angular/plugins/angular-mgonto-wizard.js
@@ -14,13 +14,15 @@ angular.module("step.html", []).run(["$templateCache", function($templateCache) 
 
 angular.module("wizard.html", []).run(["$templateCache", function($templateCache) {
     $templateCache.put("wizard.html",
-            "<div>\n" +
-            "    <ul class=\"steps-indicator col-sm-3 steps-{{steps.length}} nav nav-list\" ng-if=\"!hideIndicators\">\n" +
-            "      <li ng-class=\"{default: !step.completed && !step.selected, current: step.selected && !step.completed, done: step.completed && !step.selected, editing: step.selected && step.completed}\" ng-repeat=\"step in steps\">\n" +
-            "        <a ng-click=\"goTo(step)\"><i ng-if=\"step.icon\" class=\"{{ step.icon }}\"></i> <i class=\"fa fa-check text-success\" ng-show=\"step.completed\"></i> {{step.title || step.wzTitle}}</a>\n" +
-            "      </li>\n" +
-            "    </ul>\n" +
-            "    <div class=\"steps col-sm-9\" ng-transclude></div>\n" +
+            "<div>" +
+            "   <div class=\"left-panel col-sm-3\">\n" +
+            "       <ul class=\"left-panel-body steps-{{steps.length}} nav nav-list\" ng-if=\"!hideIndicators\">\n" +
+            "           <li ng-class=\"{default: !step.completed && !step.selected, current: step.selected && !step.completed, done: step.completed && !step.selected, editing: step.selected && step.completed}\" ng-repeat=\"step in steps\">\n" +
+            "               <a ng-click=\"goTo(step)\"><i ng-if=\"step.icon\" class=\"{{ step.icon }}\"></i> <i class=\"fa fa-check text-success\" ng-show=\"step.completed\"></i> {{step.title || step.wzTitle}}</a>\n" +
+            "           </li>\n" +
+            "       </ul>\n" +
+            "   </div>\n" +
+            "   <div class=\"right-panel steps col-sm-9\" ng-transclude></div>\n" +
             "</div>\n" +
             "");
 }]);
