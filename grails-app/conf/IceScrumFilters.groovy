544f7ad2ccafa3634f3fdc1af32260f54a8b32d8
vbarrier <vbarrier@kagilum.com>
2016-06-06 20:30:40 +0200

    test cache-control (sfr proxy shit)

--- grails-app/conf/IceScrumFilters.groovy
+++ grails-app/conf/IceScrumFilters.groovy
@@ -45,10 +45,10 @@ class IceScrumFilters {
 
         all(controller: '*', action: '*') {
             before = {
-                response.setHeader(HEADER_CACHE_CONTROL, "no-store")
-                response.setHeader(HEADER_CACHE_CONTROL, "no-cache")
-                response.setHeader(HEADER_CACHE_CONTROL, "no-transform")
-                response.setHeader(HEADER_CACHE_CONTROL, "must-revalidate")
+                response.addHeader(HEADER_CACHE_CONTROL, "no-store")
+                response.addHeader(HEADER_CACHE_CONTROL, "no-cache")
+                response.addHeader(HEADER_CACHE_CONTROL, "no-transform")
+                response.addHeader(HEADER_CACHE_CONTROL, "must-revalidate")
             }
         }
 
