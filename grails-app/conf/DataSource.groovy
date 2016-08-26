370b59d0aa75b26773260621ae25cec428aac9f9
Nicolas Noullet <nnoullet@kagilum.com>
2016-08-26 17:37:42 +0200

    Attempt to fix cache

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -24,6 +24,7 @@ hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory'
 }
 // environment specific settings
 environments {
