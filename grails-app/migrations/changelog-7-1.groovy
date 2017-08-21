2bd8f686242c9b3f547fa65bc557d633acb650c3
Nicolas Noullet <nnoullet@kagilum.com>
2017-08-21 15:17:07 +0200

    Migrations: - default value backlog chart_type - not nullable column backlog chart_type - drop column widget on_right

--- grails-app/migrations/changelog-7-1.groovy
+++ grails-app/migrations/changelog-7-1.groovy
@@ -1,10 +1,13 @@
 databaseChangeLog = {
     changeSet(author: "vbarrier", id: "add_notnull_constraint_backlog_chart_type") {
-        sql.execute("UPDATE is_backlog SET chart_type = 'type' where code = 'sandbox' OR code = 'done'")
-        sql.execute("UPDATE is_backlog SET chart_type = 'state' where code = 'backlog' OR code = 'all'")
-        addNotNullConstraint(tableName: "is_backlog", columnName: "chart_type")
+        grailsChange {
+            change {
+                sql.execute("UPDATE is_backlog SET chart_type = 'type' where code = 'sandbox' OR code = 'done'")
+                sql.execute("UPDATE is_backlog SET chart_type = 'state' where code = 'backlog' OR code = 'all'")
+            }
+        }
+        addNotNullConstraint(tableName: "is_backlog", columnName: "chart_type", columnDataType: "varchar(255)")
     }
-
     changeSet(author: "vbarrier", id: "drop_on_right_columnn") {
         preConditions(onFail: 'MARK_RAN') {
             columnExists(columnName: "on_right", tableName: "is_up_widgets")
