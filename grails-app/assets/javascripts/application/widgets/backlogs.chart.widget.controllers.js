7965b45996817c1eee79979bd7c5ae99df882d11
Nicolas Noullet <nnoullet@kagilum.com>
2017-08-23 14:43:52 +0200

    Add missing copyright headers

--- grails-app/assets/javascripts/application/widgets/backlogs.chart.widget.controllers.js
+++ grails-app/assets/javascripts/application/widgets/backlogs.chart.widget.controllers.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2017 Kagilum SAS.
+ *
+ * This file is part of iceScrum.
+ *
+ * iceScrum is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * iceScrum is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with iceScrum.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *
+ * Vincent Barrier (vbarrier@kagilum.com)
+ * Nicolas Noullet (nnoullet@kagilum.com)
+ *
+ */
+
 controllers.controller('backlogChartWidgetCtrl', ['$scope', 'BacklogService', 'ProjectService', '$controller', '$element', function($scope, BacklogService, ProjectService, $controller, $element) {
     $controller('chartWidgetCtrl', {$scope: $scope, $element: $element});
     var widget = $scope.widget; // $scope.widget is inherited
