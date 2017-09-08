75afa88edf69860dd11b1f92cd01da7f710d78e5
Vincent Barrier <vbarrier@kagilum.com>
2017-09-08 17:36:33 +0200

    - fix error on push - fix error when delete a story with task and build / commits - improve (try) report

--- grails-app/controllers/org/icescrum/web/presentation/ErrorsController.groovy
+++ grails-app/controllers/org/icescrum/web/presentation/ErrorsController.groovy
@@ -114,13 +114,14 @@ class ErrorsController implements ControllerErrorHandler {
                                     bcc    : admins*.email.toArray(),
                                     subject: "[iceScrum][report] Error report v7",
                                     view   : '/emails-templates/reportError',
-                                    model  : [params    : params,
-                                              version   : g.meta(name: 'app.version'),
-                                              stackTrace: ExceptionUtils.getStackTrace(exception),
-                                              message   : exception.message,
-                                              appID     : grailsApplication.config.icescrum.appID,
-                                              ip        : request.getHeader('X-Forwarded-For') ?: request.getRemoteAddr(),
-                                              user      : user ? user.username + ' - ' + user.firstName + ' ' + user.lastName + ' - ' + user.email : 'Not logged in'],
+                                    model  : [uriParams     : params,
+                                              uri           : request.forwardURI,
+                                              version       : g.meta(name: 'app.version'),
+                                              stackTrace    : ExceptionUtils.getStackTrace(exception),
+                                              message       : exception.message,
+                                              appID         : grailsApplication.config.icescrum.appID,
+                                              ip            : request.getHeader('X-Forwarded-For') ?: request.getRemoteAddr(),
+                                              user          : user ? user.username + ' - ' + user.firstName + ' ' + user.lastName + ' - ' + user.email : 'Not logged in'],
                                     async  : false
                             ]);
                             returnError(code: 'is.error.and.message.sent', exception: exception)
