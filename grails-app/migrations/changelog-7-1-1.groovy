875b35c2ed6a7198ae623ba401b87155937b3f63
Vincent Barrier <vbarrier@kagilum.com>
2017-09-14 01:24:56 +0200

    add new comments / attachments count management

--- grails-app/migrations/changelog-7-1-1.groovy
+++ grails-app/migrations/changelog-7-1-1.groovy
@@ -32,39 +32,48 @@ databaseChangeLog = {
     changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count") {
         grailsChange {
             change {
+                sql.execute("UPDATE is_task SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
+                sql.execute("UPDATE is_story SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
+                sql.execute("UPDATE is_feature SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
+
+                sql.execute("UPDATE is_sprint SET attachments_count = 0  WHERE attachments_count IS NULL")
+                sql.execute("UPDATE is_project SET attachments_count = 0  WHERE attachments_count IS NULL")
+                sql.execute("UPDATE is_release SET attachments_count = 0  WHERE attachments_count IS NULL")
+
+
                 println "Startinng attachments / comments count migration (can take time!)"
                 println "features..."
                 def features = Feature.getAll()
                 features.each {
                     it.attachments_count = it.getTotalAttachments()
                     it.comments_count = it.getTotalComments()
-                    it.save(failOnError:true)
+                    it.save(flush:it == features.last(), failOnError:true)
                 }
                 println "stories..."
                 def stories = Story.getAll()
                 stories.each {
                     it.attachments_count = it.getTotalAttachments()
                     it.comments_count = it.getTotalComments()
-                    it.save(failOnError:true)
+                    it.save(flush:it == stories.last(), failOnError:true)
                 }
                 println "tasks..."
                 def tasks = Task.getAll()
                 tasks.each {
                     it.attachments_count = it.getTotalAttachments()
                     it.comments_count = it.getTotalComments()
-                    it.save(failOnError:true)
+                    it.save(flush:it == tasks.last(), failOnError:true)
                 }
                 println "releases..."
                 def releases = Release.getAll()
                 releases.each {
                     it.attachments_count = it.getTotalAttachments()
-                    it.save(failOnError:true)
+                    it.save(flush:it == releases.last(), failOnError:true)
                 }
                 println "sprints..."
                 def sprints = Sprint.getAll()
                 sprints.each {
                     it.attachments_count = it.getTotalAttachments()
-                    it.save(failOnError:true)
+                    it.save(flush:it == sprints.last(), failOnError:true)
                 }
                 println "projects..."
                 def projects = Project.getAll()
