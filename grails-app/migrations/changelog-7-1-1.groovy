77f679e2a43b6d84a98797ec99a20e4950c5107c
Vincent Barrier <vbarrier@kagilum.com>
2017-09-14 03:01:36 +0200

    breakdown changelog

--- grails-app/migrations/changelog-7-1-1.groovy
+++ grails-app/migrations/changelog-7-1-1.groovy
@@ -29,19 +29,11 @@ import org.icescrum.core.domain.Task
 *
 */
 databaseChangeLog = {
-    changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count") {
+    changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count_is_feature") {
         grailsChange {
             change {
-                sql.execute("UPDATE is_task SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
-                sql.execute("UPDATE is_story SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
-                sql.execute("UPDATE is_feature SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
-
-                sql.execute("UPDATE is_sprint SET attachments_count = 0  WHERE attachments_count IS NULL")
-                sql.execute("UPDATE is_project SET attachments_count = 0  WHERE attachments_count IS NULL")
-                sql.execute("UPDATE is_release SET attachments_count = 0  WHERE attachments_count IS NULL")
-
-
                 println "Starting attachments / comments count migration (It will take some time!)"
+                sql.execute("UPDATE is_feature SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
                 println "features..."
                 def features = Feature.getAll()
                 features.each {
@@ -49,6 +41,16 @@ databaseChangeLog = {
                     it.comments_count = it.getTotalComments()
                     it.save(flush:it == features.last(), failOnError:true)
                 }
+            }
+        }
+        addNotNullConstraint(tableName: "is_feature", columnName: "comments_count", columnDataType: "integer")
+        addNotNullConstraint(tableName: "is_feature", columnName: "attachments_count", columnDataType: "integer")
+    }
+
+    changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count_is_story") {
+        grailsChange {
+            change {
+                sql.execute("UPDATE is_story SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
                 println "stories..."
                 def stories = Story.getAll()
                 stories.each {
@@ -56,6 +58,16 @@ databaseChangeLog = {
                     it.comments_count = it.getTotalComments()
                     it.save(flush:it == stories.last(), failOnError:true)
                 }
+            }
+        }
+        addNotNullConstraint(tableName: "is_story", columnName: "comments_count", columnDataType: "integer")
+        addNotNullConstraint(tableName: "is_story", columnName: "attachments_count", columnDataType: "integer")
+    }
+
+    changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count_is_task") {
+        grailsChange {
+            change {
+                sql.execute("UPDATE is_task SET comments_count = 0, attachments_count = 0  WHERE comments_count IS NULL OR attachments_count IS NULL")
                 println "tasks..."
                 def tasks = Task.getAll()
                 tasks.each {
@@ -63,18 +75,45 @@ databaseChangeLog = {
                     it.comments_count = it.getTotalComments()
                     it.save(flush:it == tasks.last(), failOnError:true)
                 }
-                println "releases..."
-                def releases = Release.getAll()
-                releases.each {
-                    it.attachments_count = it.getTotalAttachments()
-                    it.save(flush:it == releases.last(), failOnError:true)
-                }
+            }
+        }
+        addNotNullConstraint(tableName: "is_task", columnName: "comments_count", columnDataType: "integer")
+        addNotNullConstraint(tableName: "is_task", columnName: "attachments_count", columnDataType: "integer")
+    }
+
+    changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count_is_sprint") {
+        grailsChange {
+            change {
+                sql.execute("UPDATE is_sprint SET attachments_count = 0  WHERE attachments_count IS NULL")
                 println "sprints..."
                 def sprints = Sprint.getAll()
                 sprints.each {
                     it.attachments_count = it.getTotalAttachments()
                     it.save(flush:it == sprints.last(), failOnError:true)
                 }
+            }
+        }
+        addNotNullConstraint(tableName: "is_sprint", columnName: "attachments_count", columnDataType: "integer")
+    }
+
+    changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count_is_release") {
+        grailsChange {
+            change {
+               sql.execute("UPDATE is_release SET attachments_count = 0  WHERE attachments_count IS NULL")
+                println "releases..."
+                def releases = Release.getAll()
+                releases.each {
+                    it.attachments_count = it.getTotalAttachments()
+                    it.save(flush:it == releases.last(), failOnError:true)
+                }
+            }
+        }
+        addNotNullConstraint(tableName: "is_release", columnName: "attachments_count", columnDataType: "integer")
+    }
+    changeSet(author: "vbarrier", id: "add_notnull_constraint_attachments_comments_count_is_project") {
+        grailsChange {
+            change {
+                sql.execute("UPDATE is_project SET attachments_count = 0  WHERE attachments_count IS NULL")
                 println "projects..."
                 def projects = Project.getAll()
                 projects.each {
@@ -84,16 +123,6 @@ databaseChangeLog = {
                 println "migration finished!"
             }
         }
-        addNotNullConstraint(tableName: "is_task", columnName: "comments_count", columnDataType: "integer")
-        addNotNullConstraint(tableName: "is_story", columnName: "comments_count", columnDataType: "integer")
-        addNotNullConstraint(tableName: "is_feature", columnName: "comments_count", columnDataType: "integer")
-
-        addNotNullConstraint(tableName: "is_task", columnName: "attachments_count", columnDataType: "integer")
-        addNotNullConstraint(tableName: "is_story", columnName: "attachments_count", columnDataType: "integer")
-        addNotNullConstraint(tableName: "is_feature", columnName: "attachments_count", columnDataType: "integer")
-
-        addNotNullConstraint(tableName: "is_sprint", columnName: "attachments_count", columnDataType: "integer")
         addNotNullConstraint(tableName: "is_project", columnName: "attachments_count", columnDataType: "integer")
-        addNotNullConstraint(tableName: "is_release", columnName: "attachments_count", columnDataType: "integer")
     }
 }
