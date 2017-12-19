c5ef56a76b0ee60a872944cc76d9b89047439357
vbarrier <vbarrier@kagilum.com>
2017-12-19 22:06:12 +0100

    fix maxSize utf8mb4

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -315,7 +315,6 @@ grails.attachmentable.taskDir = {
 }
 
 grails.taggable.preserve.case = true
-grails.taggable.utf8mb4 = ApplicationSupport.isMySQLUTF8mb4()
 
 grails.plugin.databasemigration.updateOnStart = true
 grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
@@ -613,6 +612,7 @@ grails {
     }
     gorm.default.constraints = {
         if (ApplicationSupport.isMySQLUTF8mb4()) {
+            grails.taggable.utf8mb4 = true
             keyMaxSize(maxSize: 191)
         } else {
             keyMaxSize([:]) //default
