2249a28a15154f575c93d8d7992dea04158faad2
Nicolas Noullet <nnoullet@kagilum.com>
2018-01-11 11:07:37 +0100

    Fix atmosphere maxInactiveActivity

--- grails-app/conf/AtmosphereMeteorConfig.groovy
+++ grails-app/conf/AtmosphereMeteorConfig.groovy
@@ -10,7 +10,7 @@ servlets = [
                 initParams: [
                         "org.atmosphere.cpr.AtmosphereFramework.analytics"                           : false,
                         "org.atmosphere.interceptor.HeartbeatInterceptor.heartbeatFrequencyInSeconds": 15, // seconds
-                        "org.atmosphere.cpr.CometSupport.maxInactiveActivity"                        : 30 * 60, //30 minutes
+                        "org.atmosphere.cpr.CometSupport.maxInactiveActivity"                        : 30 * 60000, // 30 minutes
                         "org.atmosphere.cpr.broadcasterClass"                                        : "org.icescrum.atmosphere.IceScrumBroadcaster",
                         "org.atmosphere.cpr.AtmosphereInterceptor"                                   : """
                                 org.atmosphere.interceptor.IdleResourceInterceptor,
