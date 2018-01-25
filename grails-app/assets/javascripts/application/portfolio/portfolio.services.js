16dae1b5d0f02f44b44b93cd24b7b01465e8abee
vbarrier <vbarrier@kagilum.com>
2018-01-25 22:21:33 +0100

    fix merge

--- grails-app/assets/javascripts/application/portfolio/portfolio.services.js
+++
@@ -1,6 +0,0 @@
-this.getSynchronizedProjects = function(projects) {
-    return FormService.httpGet('portfolio/synchronizedProjects', {params: {projects: _.map(projects, 'id')}}, true);
-};
-this.mergePortfolios = function(portfolios) {
-    _.each(portfolios, crudMethods[IceScrumEventType.CREATE]);
-};
