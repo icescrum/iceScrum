fa958e7c6b9dce6bf4aa4fbf865f57aff2ddf3c3
Nicolas Noullet <nnoullet@kagilum.com>
2018-02-09 15:40:13 +0100

    Fix export all projects

--- grails-app/controllers/org/icescrum/web/presentation/windows/ProjectController.groovy
+++ grails-app/controllers/org/icescrum/web/presentation/windows/ProjectController.groovy
@@ -609,7 +609,7 @@ class ProjectController implements ControllerErrorHandler {
         render(status: 200, template: "dialogs/import")
     }
 
-    @Secured(['scrumMaster() or productOwner()'])
+    @Secured(["hasRole('ROLE_ADMIN') or scrumMaster() or productOwner()"])
     def exportDialog() {
         render(status: 200, template: "dialogs/export")
     }
