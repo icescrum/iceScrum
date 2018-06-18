8808fe7224fd1e36ba4b4ebb88ca6eae6af32be2
vbarrier <vbarrier@kagilum.com>
2018-06-18 18:49:57 +0200

    update tmpdir

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -390,17 +390,9 @@ environments {
     }
 }
 
-config.icescrum.tmp.dir = System.getProperty("java.io.tmpdir") ?: false
-if(config.icescrum.tmp.dir){
-    println "\n| Tmp directory: ${config.icescrum.tmp.dir}"
-}
+println "\n| Tmp directory: ${System.getProperty("java.io.tmpdir")}"
 
 grails.cache.config = {
-    if(config.icescrum.tmp.dir){
-        diskStore {
-            path config.icescrum.tmp.dir
-        }
-    }
     cache {
         name 'feed'
         timeToLiveSeconds 120
