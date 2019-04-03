7ea731dd513155507fc39a692fe0a00dc2c31055
Nicolas Noullet <nnoullet@kagilum.com>
2019-04-03 15:32:25 +0200

    various stuff

--- grails-app/assets/javascripts/vendors/angular/plugins/angular-ui-bootstrap.js
+++ grails-app/assets/javascripts/vendors/angular/plugins/angular-ui-bootstrap.js
@@ -7556,8 +7556,8 @@ angular.module("uib/template/rating/rating.html", []).run(["$templateCache", fun
 
 angular.module("uib/template/tabs/tab.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("uib/template/tabs/tab.html",
-    "<li ng-class=\"[{active: active, disabled: disabled}, classes]\" class=\"uib-tab nav-item\">\n" +
-    "  <a href ng-click=\"select($event)\" class=\"nav-link\" uib-tab-heading-transclude>{{heading}}</a>\n" +
+    "<li class=\"uib-tab nav-item\">\n" +
+    "  <a ng-class=\"[{active: active, disabled: disabled}, classes]\" href ng-click=\"select($event)\" class=\"nav-link\" uib-tab-heading-transclude>{{heading}}</a>\n" +
     "</li>\n" +
     "");
 }]);
