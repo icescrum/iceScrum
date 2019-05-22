e290ace8f1dd68bf51b024710967df6a77a65fad
vbarrier <vbarrier@kagilum.com>
2019-05-22 12:39:17 +0200

    fix button lazy thing (damn! damn!)

--- grails-app/assets/javascripts/application/constants.js
+++ grails-app/assets/javascripts/application/constants.js
@@ -23,7 +23,7 @@
  *
  */
 function isTouchOnlyDevice() { //not the best place... but there isn't a best place for that
-    return /iP(ad|hone|od)/.test(navigator.userAgent) || navigator.userAgent.indexOf('Android') > 0
+    return /iP(ad|hone|od)/.test(navigator.userAgent) || navigator.userAgent.indexOf('Android') > 0;
 }
 
 isApplication
