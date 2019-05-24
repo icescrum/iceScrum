694220177bd1dee3ef5bc1dd1238ad8406266363
Nicolas Noullet <nnoullet@kagilum.com>
2019-05-24 14:21:26 +0200

    Fix merge issue

--- grails-app/assets/javascripts/application/constants.js
+++ grails-app/assets/javascripts/application/constants.js
@@ -22,9 +22,6 @@
  * Colin Bontemps (cbontemps@kagilum.com)
  *
  */
-function isTouchOnlyDevice() { //not the best place... but there isn't a best place for that
-    return /iP(ad|hone|od)/.test(navigator.userAgent) || navigator.userAgent.indexOf('Android') > 0;
-}
 
 isApplication
     .constant('SERVER_ERRORS', {
