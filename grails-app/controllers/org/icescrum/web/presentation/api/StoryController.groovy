4a563717529d3251c8a73a2681393e3dc4cd2cc4
vbarrier <vbarrier@kagilum.com>
2019-06-25 08:46:22 +0200

    preserve order multiple stories when backlog -> sandbox

--- grails-app/controllers/org/icescrum/web/presentation/api/StoryController.groovy
+++ grails-app/controllers/org/icescrum/web/presentation/api/StoryController.groovy
@@ -338,7 +338,7 @@ class StoryController implements ControllerErrorHandler {
             rank = params.story.rank instanceof Number ? params.story.rank : params.story.rank.toInteger()
         }
         Story.withTransaction {
-            stories.each { Story story ->
+            stories.sort { a, b -> b.rank <=> a.rank }.each { Story story ->
                 storyService.returnToSandbox(story, rank)
             }
         }
