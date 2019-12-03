7da0345a8e06bf7a919fc93f0af07db01f4a9b3c
vbarrier <vbarrier@kagilum.com>
2019-12-03 14:28:21 +0100

    fix checkbox deeeeeep

--- grails-app/assets/javascripts/application/directives.js
+++ grails-app/assets/javascripts/application/directives.js
@@ -106,10 +106,25 @@ directives.directive('isMarkitup', ['$http', '$rootScope', function($http, $root
             element.on('focus', function(event) {
                 var options = scope.markitupCheckbox;
                 if (options.isEnabled()) {
+
                     var editable = options.object();
                     event.stopPropagation();
                     var isChecked = element.hasClass('fa-check-square-o');
-                    var indexClicked = element.parent().find(isChecked ? '[markitup-checkbox].fa-check-square-o' : '[markitup-checkbox].fa-square-o').index(element[0]) + 1;
+                    var securityCount = 0;
+                    var currentParent = element.parent();
+                    while(!currentParent.hasClass('markitup-preview')){
+                        currentParent = currentParent.parent();
+                        securityCount++;
+                        if(securityCount === 20){ //no more than 20 iterations to find the markitup preview
+                            element.off('focus');
+                            return;
+                        }
+                    }
+                    if(!currentParent.hasClass('markitup-preview')){
+                        element.off('focus');
+                        return;
+                    }
+                    var indexClicked = currentParent.find(isChecked ? '[markitup-checkbox].fa-check-square-o' : '[markitup-checkbox].fa-square-o').index(element[0]) + 1;
                     var textile = editable[options.property];
                     var indexCheckbox = 0;
                     textile = textile.replace(isChecked ? /\[x\]/g : /\[\]/g, function(match) {
