2e05e2a2b8799b99bbe185dc55033f8c2502afd4
Nicolas Noullet <nnoullet@kagilum.com>
2020-03-16 16:05:08 +0100

    Unique topic

--- grails-app/assets/javascripts/application/meeting/meeting.controller.js
+++ grails-app/assets/javascripts/application/meeting/meeting.controller.js
@@ -27,11 +27,13 @@ extensibleController('meetingCtrl', ['$scope', '$injector', 'AppService', 'Meeti
         if (provider.enabled) {
             var meeting = new Meeting();
             meeting.provider = provider.id;
+            var uniqueIdentifier = Math.random().toString(36).substring(2, 10);
             if (subject) {
                 meeting.topic = $scope.message('is.' + subject.class.toLowerCase()) + (subject.uid ? ' ' + subject.uid + ' ' : '') + ' - ' + subject.name;
             } else {
                 meeting.topic = $scope.message('is.ui.collaboration.meeting.default');
             }
+            meeting.topic += (' (' + uniqueIdentifier + ')');
             meeting.startDate = moment().format();
             if (subject) {
                 meeting.subjectId = subject.id;
