a181de2adfee3a93a51035d8368b716061946344
vbarrier <vbarrier@kagilum.com>
2020-04-01 21:40:11 +0200

    fix

--- grails-app/assets/javascripts/application/attachment/attachment.controllers.js
+++ grails-app/assets/javascripts/application/attachment/attachment.controllers.js
@@ -54,7 +54,10 @@ extensibleController('attachmentCtrl', ['$scope', '$uibModal', '$injector', 'Att
     $scope.getMethod = function(attachment, method) {
         var methodExt = $scope[method + _.capitalize(attachment.provider) + _.capitalize(attachment.ext)];
         var methodWithoutExt = $scope[method + _.capitalize(attachment.provider)];
-        var methodProviderConfig = _.find($scope.getAttachmentProviders(), ['name', attachment.provider])[method];
+        var provider = _.find($scope.getAttachmentProviders(), function(p) {
+            return p.id == attachment.provider || p.name || attachment.provider
+        });
+        var methodProviderConfig = provider ? provider[method] : null;
         return methodExt ? methodExt : (methodWithoutExt ? methodWithoutExt : (methodProviderConfig ? methodProviderConfig : null));
     };
     $scope.getUrl = function(clazz, attachmentable, attachment) {
