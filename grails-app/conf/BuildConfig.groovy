4bc58e2efec8642285c440ae055bd3c9fbd9d261
vbarrier <vbarrier@kagilum.com>
2022-02-28 16:35:30 +0100

    upgrade grails to 2.5.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -76,8 +76,10 @@ grails.project.dependency.resolution = {
         grailsCentral()
         grailsHome()
         mavenCentral()
-        mavenRepo "https://jcenter.bintray.com" // For http-builder 7.2 (unavailable in maven central)` <- For some reasons, inline icescrum-core requires icescrum to have some of its repositories
-        mavenRepo "https://repo.eclipse.org/content/repositories/mylyn/" // For wikitext <- For some reasons, inline icescrum-core requires icescrum to have some of its repositories
+        mavenRepo "https://jcenter.bintray.com"
+        // For http-builder 7.2 (unavailable in maven central)` <- For some reasons, inline icescrum-core requires icescrum to have some of its repositories
+        mavenRepo "https://repo.eclipse.org/content/repositories/mylyn/"
+        // For wikitext <- For some reasons, inline icescrum-core requires icescrum to have some of its repositories
         mavenRepo("https://repo.icescrum.org/artifactory/plugins-release/") {
             updatePolicy "interval:1"
         }
@@ -86,9 +88,12 @@ grails.project.dependency.resolution = {
         }
     }
     dependencies {
-        build("com.lowagie:itext:2.1.7") { excludes "bouncycastle:bcprov-jdk14:138", "org.bouncycastle:bcprov-jdk14:1.38" } //not needed by icescrum
-        runtime("com.lowagie:itext:2.1.7") { excludes "bouncycastle:bcprov-jdk14:138", "org.bouncycastle:bcprov-jdk14:1.38" } //not needed by icescrum
-        compile("com.lowagie:itext:2.1.7") { excludes "bouncycastle:bcprov-jdk14:138", "org.bouncycastle:bcprov-jdk14:1.38" } //not needed by icescrum
+        build("com.lowagie:itext:2.1.7") { excludes "bouncycastle:bcprov-jdk14:138", "org.bouncycastle:bcprov-jdk14:1.38" }
+        //not needed by icescrum
+        runtime("com.lowagie:itext:2.1.7") { excludes "bouncycastle:bcprov-jdk14:138", "org.bouncycastle:bcprov-jdk14:1.38" }
+        //not needed by icescrum
+        compile("com.lowagie:itext:2.1.7") { excludes "bouncycastle:bcprov-jdk14:138", "org.bouncycastle:bcprov-jdk14:1.38" }
+        //not needed by icescrum
         runtime 'mysql:mysql-connector-java:5.1.49'
         compile 'commons-fileupload:commons-fileupload:1.3.3' //fix CVE-2016-1000031
         compile 'org.apache.logging.log4j:log4j-api:2.17.2'
@@ -99,7 +104,9 @@ grails.project.dependency.resolution = {
     plugins {
         compile ':cache-headers:1.1.7'
         compile ':asset-pipeline:2.14.1.1'
-        compile 'org.icescrum:sass-asset-pipeline:2.14.3'
+        compile('org.icescrum:sass-asset-pipeline:2.14.3') {
+            excludes 'log4j'
+        }
         compile ':browser-detection:2.9.0'
         runtime ':hibernate4:4.3.10'
         runtime 'org.grails.plugins:database-migration:1.4.1'
