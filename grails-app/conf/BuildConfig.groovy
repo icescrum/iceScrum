1e30b7e3fe53205ab8fdc682d3ea4550b7244c01
Nicolas Noullet <nnoullet@kagilum.com>
2022-04-12 12:53:35 +0200

    Update standalone

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -115,7 +115,7 @@ grails.project.dependency.resolution = {
         compile 'org.icescrum:entry-points:1.4.2'
         if (Environment.current == Environment.PRODUCTION) {
             compile 'org.icescrum:icescrum-core:1.7-SNAPSHOT'
-            compile 'org.icescrum:standalone:8.5.51.17'
+            compile 'org.icescrum:standalone:8.5.78'
         }
 //        if (Environment.current == Environment.DEVELOPMENT) {
 //            compile ":hibernate-metrics:0.1.1"
